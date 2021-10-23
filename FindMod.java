914d40c011ab56ca348264e958523fd238f540c6
Ishwari Pednekar <58209938+itpednekar@users.noreply.github.com>
2021-10-23 12:35:18 +0530

    Update FindMod.java

--- FindMod.java
+++ FindMod.java
@@ -1,7 +1,7 @@
 //Problem asked in google
 public class MyClass {
     public static void main(String args[]) {
-        String str = " ";
+        String str = "24587965";
         int p = 7;
         Integer mod = 0;
         for(int i=0; i<str.length(); i++){
