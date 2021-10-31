b93906843fbc42921104dfbfcf767691caf82b7e
Ishwari Pednekar <58209938+itpednekar@users.noreply.github.com>
2021-10-31 19:30:27 +0530

    Update SubarrayOR.java

--- SubarrayOR.java
+++ SubarrayOR.java
@@ -1,36 +1,32 @@
+//https://github.com/itpednekar/Scaler-Arrays/blob/main/SubarrayOR.java
 public class Solution {
-    public int solve(ArrayList<Integer> A) {
-        int n = A.size();
-        long mod = 1000 * 1000 * 1000 + 7;
+    public int solve(int[] A) {
+        long n = A.length, mod = 1000 * 1000 * 1000 + 7;
+        long pow = 1L; long ans = 0L;
         long totalSubarr = n * (n + 1) / 2;
-        long ans = 0L;
-        long pow = 1L;
-        
+
         for(int i=0; i<32; i++){
             long contiZero = 0L;
             long subArrWithORZero = 0L;
             long subArrWithOROne = 0L;
+
             for(int j=0; j<n; j++){
-                int ithBitSet = (A.get(j) & (1 << i)) == 0 ? 0 : 1;
-                if(ithBitSet == 0)
-                    contiZero++;
-                else{
+                if((A[j] & (1 << i)) == (1 << i)){
                     subArrWithORZero = subArrWithORZero + (contiZero * (contiZero + 1)) / 2;
                     contiZero = 0;
                 }
+                else{
+                    contiZero++;
+                }
             }
-            subArrWithORZero = subArrWithORZero + (contiZero * (contiZero + 1) / 2);
-            subArrWithOROne = totalSubarr - subArrWithORZero;
-            
+            subArrWithORZero += (contiZero*(contiZero + 1)) / 2;
+            long oneCount = totalSubarr - subArrWithORZero;
             if(i != 0){
-                pow = pow % mod;
                 pow = pow * 2L;
             }
-            
-            ans += (subArrWithOROne * pow) % mod;
-            ans = ans % mod;
+            ans += (oneCount*pow) % mod;
+            ans %= mod;
         }
-        
-        return (int) ans ;
+        return (int) ans;
     }
 }
